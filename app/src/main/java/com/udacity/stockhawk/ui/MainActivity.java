ae8a4feaacbf43b77182addbce7012631273b57d
angelone191 <angelone191@gmail.com>
2018-02-28 13:27:03 +0800

    done issue #1 using app with screen reader

--- app/src/main/java/com/udacity/stockhawk/ui/MainActivity.java
+++ app/src/main/java/com/udacity/stockhawk/ui/MainActivity.java
@@ -156,13 +156,14 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         adapter.setCursor(null);
     }
 
-
     private void setDisplayModeMenuItemIcon(MenuItem item) {
         if (PrefUtils.getDisplayMode(this)
                 .equals(getString(R.string.pref_display_mode_absolute_key))) {
             item.setIcon(R.drawable.ic_percentage);
+            item.setTitle(R.string.pref_display_mode_percentage_key);
         } else {
             item.setIcon(R.drawable.ic_dollar);
+            item.setTitle(R.string.pref_display_mode_absolute_key);
         }
     }
 
