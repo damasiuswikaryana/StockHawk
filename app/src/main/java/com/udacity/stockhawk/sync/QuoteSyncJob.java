8e0f8ac07aabfa10cffe34a2217d0c4878943961
angelone191 <angelone191@gmail.com>
2018-02-28 20:31:53 +0800

    adding widget and create status message

--- app/src/main/java/com/udacity/stockhawk/sync/QuoteSyncJob.java
+++ app/src/main/java/com/udacity/stockhawk/sync/QuoteSyncJob.java
@@ -10,6 +10,7 @@ import android.icu.math.BigDecimal;
 import android.icu.text.SimpleDateFormat;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.widget.Toast;
 
 import com.udacity.stockhawk.data.Contract;
 import com.udacity.stockhawk.data.PrefUtils;
@@ -40,7 +41,8 @@ import okhttp3.Response;
 import timber.log.Timber;
 
 public final class QuoteSyncJob {
-    private static final String ACTION_DATA_UPDATED = "com.udacity.stockhawk.ACTION_DATA_UPDATED";
+
+    public static final String ACTION_DATA_UPDATED = "com.udacity.stockhawk.ACTION_DATA_UPDATED";
 
     private static final int ONE_OFF_ID = 2;
     private static final int PERIOD = 300000;
@@ -54,53 +56,63 @@ public final class QuoteSyncJob {
     private static final LocalDate startDate = LocalDate.now().minus(YEARS_OF_HISTORY, ChronoUnit.YEARS);
     private static final LocalDate endDate = LocalDate.now();
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
     private static StringBuilder historyBuilder = new StringBuilder();
 
-    private QuoteSyncJob() { }
+    private QuoteSyncJob() {
+    }
 
     static HttpUrl createQuery(String symbol) {
-        HttpUrl.Builder httpUrl = HttpUrl.parse(QUANDL_ROOT+symbol+".json").newBuilder();
+        HttpUrl.Builder httpUrl = HttpUrl.parse(QUANDL_ROOT + symbol + ".json").newBuilder();
         httpUrl.addQueryParameter("column_index", "4")  //closing price
                 .addQueryParameter("start_date", formatter.format(startDate))
                 .addQueryParameter("end_date", formatter.format(endDate));
         return httpUrl.build();
     }
 
-    static ContentValues processStock(JSONObject jsonObject) throws JSONException{
+    static ContentValues processStock(Context context, JSONObject jsonObject) throws JSONException {
         String stockSymbol = jsonObject.getString("dataset_code");
-
-        JSONArray historicData = jsonObject.getJSONArray("data");
-
-        double price = historicData.getJSONArray(0).getDouble(1);
-        double change = price - historicData.getJSONArray(1).getDouble(1);
-        double percentChange = 100 * (( price - historicData.getJSONArray(1).getDouble(1) ) / historicData.getJSONArray(1).getDouble(1));
-
-        historyBuilder = new StringBuilder();
-
-        for (int i = 0; i<historicData.length(); i++) {
-            JSONArray array = historicData.getJSONArray(i);
-            // Append date
-            historyBuilder.append(array.get(0));
-            historyBuilder.append(", ");
-            // Append close
-            historyBuilder.append(array.getDouble(1));
-            historyBuilder.append("\n");
-        }
-
         ContentValues quoteCV = new ContentValues();
-        quoteCV.put(Contract.Quote.COLUMN_SYMBOL, stockSymbol);
-        quoteCV.put(Contract.Quote.COLUMN_PRICE, price);
-        quoteCV.put(Contract.Quote.COLUMN_PERCENTAGE_CHANGE, percentChange);
-        quoteCV.put(Contract.Quote.COLUMN_ABSOLUTE_CHANGE, change);
-        quoteCV.put(Contract.Quote.COLUMN_HISTORY, historyBuilder.toString());
+
+        try {
+            JSONArray historicData = jsonObject.getJSONArray("data");
+
+            double price = historicData.getJSONArray(0).getDouble(1);
+            double change = price - historicData.getJSONArray(1).getDouble(1);
+            double percentChange = 100 * ((price - historicData.getJSONArray(1).getDouble(1)) / historicData.getJSONArray(1).getDouble(1));
+
+            historyBuilder = new StringBuilder();
+
+            for (int i = 0; i < historicData.length(); i++) {
+                JSONArray array = historicData.getJSONArray(i);
+
+                // Append date
+                historyBuilder.append(array.get(0));
+                historyBuilder.append(", ");
+
+                // Append close
+                historyBuilder.append(array.getDouble(1));
+                historyBuilder.append("\n");
+            }
+
+            quoteCV.put(Contract.Quote.COLUMN_SYMBOL, stockSymbol);
+            quoteCV.put(Contract.Quote.COLUMN_PRICE, price);
+            quoteCV.put(Contract.Quote.COLUMN_PERCENTAGE_CHANGE, percentChange);
+            quoteCV.put(Contract.Quote.COLUMN_ABSOLUTE_CHANGE, change);
+            quoteCV.put(Contract.Quote.COLUMN_HISTORY, historyBuilder.toString());
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+            PrefUtils.removeStock(context, stockSymbol);
+            QuoteStatus.setStatus(context, QuoteStatus.STOCK_INVALID);
+        }
 
         return quoteCV;
     }
 
     static void getQuotes(final Context context) {
         Timber.d("Running sync job");
-        historyBuilder = new StringBuilder();
 
+        historyBuilder = new StringBuilder();
         try {
             Set<String> stockPref = PrefUtils.getStocks(context);
 
@@ -112,6 +124,7 @@ public final class QuoteSyncJob {
                     @Override
                     public void onFailure(Call call, IOException e) {
                         Timber.e("OKHTTP", e.getMessage());
+                        QuoteStatus.setStatus(context, QuoteStatus.STOCK_SERVER_DOWN);
                     }
 
                     @Override
@@ -119,17 +132,25 @@ public final class QuoteSyncJob {
                         try {
                             String body = response.body().string();
                             JSONObject jsonObject = new JSONObject(body);
-                            ContentValues quotes = processStock(jsonObject.getJSONObject("dataset"));
 
-                            context.getContentResolver().insert(Contract.Quote.URI,quotes);
-                        } catch(JSONException ex){}
+                            if (jsonObject.has("dataset")) {
+                                ContentValues quotes = processStock(context, jsonObject.getJSONObject("dataset"));
+                                context.getContentResolver().insert(Contract.Quote.URI, quotes);
+                            } else {
+                                context.getContentResolver().insert(Contract.Quote.URI, null);
+                                QuoteStatus.setStatus(context, QuoteStatus.STOCK_SERVER_LIMIT);
+                            }
+
+                        } catch (JSONException ex) {
+                            ex.printStackTrace();
+                            QuoteStatus.setStatus(context, QuoteStatus.STOCK_SERVER_INVALID);
+                        }
                     }
                 });
             }
 
             Intent dataUpdatedIntent = new Intent(ACTION_DATA_UPDATED);
             context.sendBroadcast(dataUpdatedIntent);
-
         } catch (Exception exception) {
             Timber.e(exception, "Error fetching stock quotes");
         }
@@ -139,7 +160,6 @@ public final class QuoteSyncJob {
         Timber.d("Scheduling a periodic task");
 
         JobInfo.Builder builder = new JobInfo.Builder(PERIODIC_ID, new ComponentName(context, QuoteJobService.class));
-
         builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
                 .setPeriodic(PERIOD)
                 .setBackoffCriteria(INITIAL_BACKOFF, JobInfo.BACKOFF_POLICY_EXPONENTIAL);
@@ -148,30 +168,24 @@ public final class QuoteSyncJob {
         scheduler.schedule(builder.build());
     }
 
-
     public static synchronized void initialize(final Context context) {
         schedulePeriodic(context);
         syncImmediately(context);
     }
 
     public static synchronized void syncImmediately(Context context) {
-
-        ConnectivityManager cm =
-                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = cm.getActiveNetworkInfo();
-        if (networkInfo != null && networkInfo.isConnectedOrConnecting()) {
 
+        if (networkInfo != null && networkInfo.isConnectedOrConnecting()) {
             Intent nowIntent = new Intent(context, QuoteIntentService.class);
             context.startService(nowIntent);
         } else {
-
             JobInfo.Builder builder = new JobInfo.Builder(ONE_OFF_ID, new ComponentName(context, QuoteJobService.class));
-
             builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
                     .setBackoffCriteria(INITIAL_BACKOFF, JobInfo.BACKOFF_POLICY_EXPONENTIAL);
 
             JobScheduler scheduler = (JobScheduler) context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
-
             scheduler.schedule(builder.build());
         }
     }
